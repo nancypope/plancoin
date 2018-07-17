2b0e6be89aaff1ff9275dee176c37fb043403af2
Nancy Pope <nancypope@protonmail.com>
2018-07-18 04:01:21 +0700

    Add build script

---
+++ make_x64.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export PATH=/home/dev/mxe/usr/bin:$PATH
+MXE_INCLUDE_PATH=/home/dev/mxe/usr/x86_64-w64-mingw32.static/include
+MXE_LIB_PATH=/home/dev/mxe/usr/x86_64-w64-mingw32.static/lib
+
+x86_64-w64-mingw32.static-qmake-qt5 \
+        BOOST_LIB_SUFFIX=-mt \
+        BOOST_THREAD_LIB_SUFFIX=_win32-mt \
+        BOOST_INCLUDE_PATH=$MXE_INCLUDE_PATH/boost \
+        BOOST_LIB_PATH=$MXE_LIB_PATH \
+        OPENSSL_INCLUDE_PATH=$MXE_INCLUDE_PATH/openssl \
+        OPENSSL_LIB_PATH=$MXE_LIB_PATH \
+        BDB_INCLUDE_PATH=$MXE_INCLUDE_PATH \
+        BDB_LIB_PATH=$MXE_LIB_PATH \
+        MINIUPNPC_INCLUDE_PATH=$MXE_INCLUDE_PATH \
+        MINIUPNPC_LIB_PATH=$MXE_LIB_PATH \
+        QMAKE_LRELEASE=/home/dev/mxe/usr/x86_64-w64-mingw32.static/qt5/bin/lrelease PLANCOIN-qt.pro
+
+make -f Makefile.Release
